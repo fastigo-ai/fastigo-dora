22813e7d2f8c2b1c58d35ae3cc568f0e4d9f83ad
Vishal Tiwari <fastigo.in@gmail.com>
2025-05-14 12:41:24 +0530

    CheckoutPage and Mackbook

--- src/pages/Services/MacbookSupport/MacbookSupport.js
+++ src/pages/Services/MacbookSupport/MacbookSupport.js
@@ -90,7 +90,7 @@ const MacbookSupport = () => {
           duration: "45 mins.",
           description:
             "Complete diagnosis to identify the issue before repair.\n Final repair cost will be shared after the diagnosis",
-          price: 1,
+          price: 699,
           image: "/assets/images/Door2fyImage/Macbook-Support/DisplayIssue.jpg",
         },
         {
